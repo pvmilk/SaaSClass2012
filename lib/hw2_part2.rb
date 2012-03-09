569dfffe396a5c8ed7610569723f779dd142bbe3
Milk Phongtharin <pvmilk@gmail.com>
2012-03-09 12:23:07 +0000

    - (Mistake last commit) Modified hw2 part2, submitted

--- lib/hw2_part2.rb
+++ lib/hw2_part2.rb
@@ -1,22 +1,17 @@
 class CartesianProduct
   include Enumerable
   # your code here
-  def initializea(a, b)
-      @r = []
-      a.each do |i|
-          b.each do |j|
-              @r << [i, j]
-
-          end
+  def initialize(a, b)
+    @r = []
+    a.each do |i|
+      b.each do |j|
+        @r << [i, j]
       end
+    end
   end
 
   def each
-      r.each do |k|
-          yield
-      end
-      {return k}
+      @r.each{|k| yield k if block_given?}
   end
-
 
 end
